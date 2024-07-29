547f4228e602cd34d709721313ffcb86b3d21928
Vidhi Rabadiya <62100375+Vidhi-Rabadiya@users.noreply.github.com>
2024-07-29 15:36:34 +0530

    Added React Basic Code

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
+
 const heading = React.createElement("h1", { id: "heading" }, "Hellow World from React");
 
+const child = React.createElement("div", { id: "child" }, heading);
+
+const parent = React.createElement("div", { id: "parent" }, child);
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
+root.render(parent);
